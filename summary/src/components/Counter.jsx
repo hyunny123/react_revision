e872e2e85514ee4c5d5db04fa7640fbb208197c8
hyunny123 <hazelc3837@gmail.com>
2023-08-25 08:31:49 +0900

    [ADD]Counter prev ing

--- summary/src/components/Counter.jsx
+++ summary/src/components/Counter.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 
-export default function Counter() {
+export default function Counter({ total, onClick }) {
   const [number, setNumber] = useState(0);
   return (
     <div className="counter">
-      <span className="number">{number}</span>
+      <span className="number">{total}</span>
       <button
         className="btn"
         onClick={() => {
-          setNumber(number + 1);
+          setNumber((prev) => prev + 1);
         }}
       >
         Add+
