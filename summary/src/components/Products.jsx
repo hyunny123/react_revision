3d5708198df8baf013bf4b18c25c0610fe895c8d
hyunny123 <hazelc3837@gmail.com>
2023-09-01 08:05:08 +0900

    [FEAT]삼항연산자사용하여 경로 변경

--- summary/src/components/Products.jsx
+++ summary/src/components/Products.jsx
@@ -3,9 +3,14 @@ import React, { useEffect, useState } from "react";
 export default function Products() {
   const [count, setCount] = useState(0);
   const [products, setProducts] = useState([]);
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = () => {
+    setChecked((prev) => !prev);
+  };
 
   useEffect(() => {
-    fetch("data/products.json")
+    fetch(`data/${checked ? "sale_" : ""}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log("데이터를 네트워크에서 받아옴");
@@ -14,10 +19,17 @@ export default function Products() {
     return () => {
       console.log("청소하는 일들을 합니다.");
     };
-  }, []);
+  }, [checked]);
 
   return (
     <>
+      <input
+        type="checkbox"
+        id="checkbox"
+        value={checked}
+        onChange={handleChange}
+      />
+      <label htmlFor="checkbox">show only! sales</label>
       <ul>
         {products.map((product) => (
           <li key={product.id}>
