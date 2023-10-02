734aa87aafeeabf12395b994de17c9b707938a99
hyunny123 <hazelc3837@gmail.com>
2023-10-02 14:54:08 +0900

    [FEAT]context darkmode 구현

--- summary/src/App.jsx
+++ summary/src/App.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from "react";
 import Header from "./components/Header/Header";
 import TodoList from "./components/TodoList/TodoList";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 const filters = ["all", "active", "completed"];
 
 export default function App() {
   const [filter, setFilter] = useState(filters[0]);
   return (
-    <div>
+    <DarkModeProvider>
       <Header
         filters={filters}
         filter={filter}
@@ -15,6 +16,6 @@ export default function App() {
         onFilterChange={setFilter}
       />
       <TodoList filter={filter} />
-    </div>
+    </DarkModeProvider>
   );
 }
---
+++ summary/src/context/DarkModeContext.jsx
@@ -0,0 +1,32 @@
+import { useContext } from "react";
+import { createContext, useState } from "react";
+
+export const DarkModecontext = createContext();
+
+//provider
+export function DarkModeProvider({ children }) {
+  const [darkMode, setDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    // setDarkMode((prev) => !prev);
+    // updateDarkMode((prev) => !prev);
+    setDarkMode(!darkMode);
+    updateDarkMode(!darkMode);
+  };
+  return (
+    <DarkModecontext.Provider value={{ darkMode, toggleDarkMode }}>
+      {children}
+    </DarkModecontext.Provider>
+  );
+}
+
+export const useDarkMode = () => useContext(DarkModecontext);
+
+function updateDarkMode(darkMode) {
+  if (darkMode) {
+    //dark라는 클래스를 넣어주기
+    document.documentElement.classList.add("dark");
+  } else {
+      //dark라는 클래스 빼주기
+    document.documentElement.classList.remove("dark");
+  }
+}
