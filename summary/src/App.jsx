1fde3f69c971b0bf9469b406239fbdcfc9be4d7b
hyunny123 <hazelc3837@gmail.com>
2023-10-05 07:56:56 +0900

    [FEAT]products setting

--- summary/src/App.jsx
+++ summary/src/App.jsx
@@ -1,28 +1,7 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import About from "./pages/About";
-import AboutDetail from "./pages/AboutDetail";
-import Home from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import Root from "./pages/Root";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    errorElement: <NotFound />,
-    children: [
-      { index: true, element: <Home /> },
-      { path: "/about", element: <About /> },
-      { path: "/about/:aboutId", element: <AboutDetail /> },
-    ],
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-]);
+import "./App.css";
+import MainProducts from "./components/MainProducts";
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return <MainProducts />;
 }
--- summary/src/pages/AboutDetail.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { useParams } from "react-router-dom";
-
-export default function AboutDetail() {
-  //   const params = useParams();
-  //   console.log(params);
-  const { aboutId } = useParams();
-  return <div>AboutDetail : {aboutId}</div>;
-}
