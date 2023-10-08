63ebff5f8dda8694a58eda3a6845227d58087551
hyunny123 <hazelc3837@gmail.com>
2023-10-08 23:12:50 +0900

    [FEAT]useQuery staleTime &cache

--- summary/src/components/Products.jsx
+++ summary/src/components/Products.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import useProducts from "../hooks/use-products";
 import { useQuery } from "@tanstack/react-query";
 
 export default function Products() {
@@ -8,12 +7,18 @@ export default function Products() {
     isLoading,
     error,
     data: products,
-  } = useQuery(["products", checked], async () => {
-    console.log("fetching...");
-    return fetch(`data/${checked ? "sale_" : ""}products.json`).then((res) =>
-      res.json()
-    );
-  });
+  } = useQuery(
+    ["products", checked],
+    async () => {
+      console.log("fetching...", checked);
+      return fetch(`data/${checked ? "sale_" : ""}products.json`).then((res) =>
+        res.json()
+      );
+    },
+    {
+      staleTime: 1000 * 60 * 5,
+    }
+  );
 
   const handleChange = () => setChecked((prev) => !prev);
 
